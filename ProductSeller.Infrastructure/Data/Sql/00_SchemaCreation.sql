b3148d30c08c07329498c3a8c148a3b2d90d66c0
Adriano Paduam de Souza <adriano.paduam@outlook.com>
2022-04-18 08:35:29 -0300

    fix: missing db columns insertion

--- ProductSeller.Infrastructure/Data/Sql/00_SchemaCreation.sql
+++ ProductSeller.Infrastructure/Data/Sql/00_SchemaCreation.sql
@@ -5,7 +5,8 @@ USE productseller;
 CREATE TABLE users(
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE products(
